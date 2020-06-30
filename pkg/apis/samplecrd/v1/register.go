ead630419161e8172cdd0c20398a0bc8f6206eef
yuewei.qyw <yuewei.qyw@alibaba-inc.com>
2020-06-30 18:06:12 +0800

    done.

--- pkg/apis/samplecrd/v1/register.go
+++ pkg/apis/samplecrd/v1/register.go
@@ -5,7 +5,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
-	"github.com/NewBeeXX/k8s-controller-custom-resource/pkg/apis/samplecrd"
+	"github.com/ywmuazz/k8s-controller-custom-resource/pkg/apis/samplecrd"
 )
 
 // GroupVersion is the identifier for the API which includes
